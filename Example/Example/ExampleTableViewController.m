bf1ef6606f14bed2f62880549525c094f54e13f1
Simon Støvring <simonstoevring@gmail.com>
2012-01-16 15:45:25 +0100

    Optimized for both iPhone and iPad

--- Example/Example/ExampleTableViewController.m
+++ Example/Example/ExampleTableViewController.m
@@ -208,7 +208,7 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 
 @end
